66c6553e96b23e97e3e032fff2d1c82244ccf2de
ogoons <ogoons@hanmail.net>
2023-09-12 22:53:37 +0900

    Init

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TimelineView"
+include(":app")
+include(":timelineview")
